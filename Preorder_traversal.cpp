19a7ca1818578c3757803d2aab15c67045723884
nicsinha <sinhanikhil097@gmail.com>
2024-02-05 07:37:40 +0530

    Pre Order traversal through iteration

--- Preorder_traversal.cpp
+++ Preorder_traversal.cpp
@@ -41,6 +41,55 @@ void postorder(Node* root) {
     postorder(root->right);
     cout<<root->data<<" ";
 }
+
+
+void levelorder(Node* root) {
+
+    queue<Node*> st;
+
+    st.push(root);
+
+    while(!st.empty()) {
+
+        int n = st.size();
+
+        for(int i=0;i<n;i++) {
+            cout<<st.front()->data<<" ";
+            Node* temp = st.front();
+            st.pop();
+
+            if(temp->left) {st.push(temp->left);}
+            if(temp->right) {st.push(temp->right);}  
+        }
+        cout<<endl;
+    }
+
+}
+
+vector<int> PreOrder_iteration(Node* root) {
+
+    vector<int> res;
+
+    stack<Node*> st;
+    st.push(root);
+
+    while(!st.empty()) {
+     
+      Node* temp = st.top();
+
+      st.pop();
+
+      res.push_back(temp->data);
+      
+      if(temp->right) {st.push(temp->right);}
+      if(temp->left) {st.push(temp->left);}
+    }
+
+    return res;
+
+}
+
+
 int main() {
 
     Node* root = new Node(1);
@@ -49,9 +98,15 @@ int main() {
     root->left->left = new Node(4);
     root->left->right = new Node(5);
     root->right->left = new Node(6);
-    preorder(root);
-    cout<<endl;
-    inorder(root);
-    cout<<endl;
-    postorder(root);
+    // preorder(root);
+    // cout<<endl;
+    // inorder(root);
+    // cout<<endl;
+    // postorder(root);
+    // cout<<endl;
+    // levelorder(root);
+
+    vector<int> v = PreOrder_iteration(root);
+
+    for(auto i:v) {cout<<i<<" ";}
 }
