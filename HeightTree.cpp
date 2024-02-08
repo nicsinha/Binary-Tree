072860252f314be95be5095428b08452ee934dfc
nicsinha <sinhanikhil097@gmail.com>
2024-02-08 22:32:28 +0530

    Find diameter

--- HeightTree.cpp
+++ HeightTree.cpp
@@ -50,6 +50,19 @@ pair<int,bool> check_balance(Node* root) {
 
 }
 
+int find_diameter(Node* root,int &diameter) {
+
+    if(root == NULL) {
+        return 0;
+    }
+
+    int lh = find_diameter(root->left,diameter);
+    int rh = find_diameter(root->right,diameter);
+
+    diameter = max(diameter,lh+rh);
+    return 1+max(lh,rh);
+}
+
 int main() {
 
     Node* root = new Node(1);
@@ -61,13 +74,15 @@ int main() {
     root->right->right = new Node(7);
     root->left->left->left = new Node(8);
     root->left->left->right = new Node(9);
-    
+    int diameter = 0;
+
+    cout<<find_diameter(root,diameter);
 
     //cout<<height(root);
 
-    pair<int,bool> ans = check_balance(root);
+    // pair<int,bool> ans = check_balance(root);
 
-    cout<<"ans = "<<ans.second;
+    // cout<<"ans = "<<ans.second;
 
     return 0;
 
